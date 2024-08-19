121c2a823f54f7190c94ee2ddf108a23e7f5a8b4
Pratham2110 <prathamhole@gmail.com>
2024-08-19 08:13:08 +0530

    snippit addded

--- snippit.cpp
+++ snippit.cpp
@@ -1,1 +1,22 @@
-git remote add origin https://github.com/Prtm2110/CP.git
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long t;
+    cin >> t;
+    while (t--) {
+        long long n;
+        cin >> n;
+
+        // set<char> re(s.begin(), s.end());
+        // vector<char> vec(re.begin(), re.end());
+
+        for (long long i = 0; i < n; i++) {
+            
+            }
+        
+
+        // cout <<  << endl;
+    }
+    return 0;
+}
