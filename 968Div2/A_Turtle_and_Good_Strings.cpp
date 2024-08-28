5ec47cb1c9c4792209dc42e8b058e210b1dc9200
Pratham2110 <prathamhole@gmail.com>
2024-08-28 12:49:32 +0530

    question added

---
+++ 968Div2/A_Turtle_and_Good_Strings.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long t;
+    cin >> t;
+    while (t--)
+    {
+        long long n;
+        cin >> n;
+        string s;
+        cin >> s;
+
+      
+        bool b = 0;
+        
+        if(s[0]==s[n-1]) cout<<"NO"<<endl;
+        else cout<<"YES"<<endl;
+    }
+    return 0;
+}
